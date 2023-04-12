340dea7584cd2e5f04f71a012a42c45ac866c203
Pavlo Kreitsberg <pavel.kreitsberg@gmail.com>
2023-04-12 13:15:11 +0300

    adding links to socials

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,10 +4,12 @@ import { SocialsContainer } from "./Socials.styled";
 function Socials() {
   return (
     <SocialsContainer>
-      <AiFillYoutube color="#051934" size="32px" />
+      
+      <a href="https://www.youtube.com/@motorhandballclub5101/featured" target="_blank"><AiFillYoutube color="#051934" size="32px" /></a>
       <a href="https://www.instagram.com/handball.club.motor/" target="_blank"><AiFillInstagram color="#051934" size="32px" /></a>
+      <a href="https://www.facebook.com/handball.club.motor/" target="_blank"><AiFillFacebook color="#051934" size="32px" /></a>
      
-      <AiFillFacebook color="#051934" size="32px" />
+      
     </SocialsContainer>
   );
 }
